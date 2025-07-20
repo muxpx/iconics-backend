d1dd32c5c415b828417543e747dc87316c8b1f73
Muxp <wertouyo0@gmail.com>
2025-07-20 05:24:02 -0400

    fix(config): implement correct Medusa v2 SSL database options

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,15 +5,18 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions: {
+      connection: { ssl: { rejectUnauthorized: false } },
+    },
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
   admin: {
     disable: true,
   },
-})
+});
